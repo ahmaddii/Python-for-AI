fe81508a1268505f3e079ef1feaebb61bb76cee2
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-11-10 23:34:38 +0500

    Python Basics enviroment

---
+++ .ipynb_checkpoints/pytaste-checkpoint.py
@@ -0,0 +1,14 @@
+name = "Ahmad"
+
+age = 25
+
+print(f"Hello my name is {name} and my age is {age}")
+
+if age >= 20 :
+
+  print("You can Vote")
+
+else :
+    print("You cant Vote")
+
+print("Hello I am learning Python with AI")
