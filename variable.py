a0550dff84065613df1c84f08e4aee30d421ce79
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-11-12 20:55:06 +0500

    variables in python

---
+++ variable.py
@@ -0,0 +1,26 @@
+# We now Understand variables in python
+
+name = "Ahmad"
+
+age = 21
+
+is_Student = True
+
+score =30
+
+print(score)
+
+score = score + 70
+
+print(score)
+
+
+print(is_Student)
+
+# I am single line comment
+
+"""
+I am multi line comments in python
+I can help you to document your code
+
+"""
