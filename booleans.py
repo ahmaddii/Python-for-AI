a93df905e1d8b00dc766932b056d97c3f72faef1
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-11-15 18:26:32 +0500

    Booleans in Python

---
+++ booleans.py
@@ -0,0 +1,29 @@
+# Booleans are only hold true or false
+
+
+is_Logged = True
+is_Admin = False
+permission = True
+
+
+print(is_Logged)
+
+age = 12
+
+
+if age >= 18 : 
+                    print("You can Vote")
+
+else: 
+        print("You can not Vote")
+
+
+# Companrision Operaters
+
+age2 = 20
+
+print(age >= 20)
+print(age <= 20)
+print(age == 20)
+print(age > 20 )
+print(age < 20)
