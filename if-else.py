48924014f053c25dd397e2e5b0a8cc94694a670f
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-11-18 22:23:27 +0500

    if else in python

---
+++ if-else.py
@@ -0,0 +1,50 @@
+age = 18
+
+if age >= 18:
+                    print("You can Vote")
+
+temp = 20
+
+if temp >= 20:
+        print("Its too Hot")
+else:
+        print("Its Too cold")
+
+# elif if
+
+score = 85
+
+if score >= 90:
+        print("A+")
+elif score >=80:
+        print("B+")
+else:
+        print("Failed")
+        
+
+age2 = 25
+has_license = True
+
+if age2 >= 18 and has_license:
+        print("You can drive")
+
+#if weekend or holiday:
+        print("Off today")
+
+#if not raining:
+        print("We can go outside")
+
+
+# nested if else
+
+has_ticket = True
+age3 = 18
+
+if has_ticket:
+        
+        if age3 >= 18:
+                print("Age is good")
+        else:
+                print("Not good")
+else:
+        print("Buy ticket first")
