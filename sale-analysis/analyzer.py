46e38a8a9d6800a731cb97788fb27b4eded5c7fa
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-11-28 00:26:29 +0500

    Play with different type of files like json csv or excel to store data in it and to manage or organize the data effeicelntly with in the code by implmenting reusable components

--- sale-analysis/analyzer.py
+++ sale-analysis/analyzer.py
@@ -1,15 +1,76 @@
+import pandas as pd
 import os
+import json
+from helpers import calculate_total,format_currency
 
-print("Current Dirctory",os.getcwd())  # current dirctory
+df = pd.read_csv("data/sales.csv")
+print("Csv Data")
+print(df)
 
-data_path = "data/sales.csv"
+print(f"\nShape: {df.shape[0]} rows, {df.shape[1]} column")
 
-if os.path.exists(data_path):
-            print(f"Path Finded  {data_path}")
 
-else:
-        print(f"Dosent Find the path {data_path}")
-        print("Plz Ensure you are in the right Dirctory")
+totals = []
+
+for index,row in df.iterrows():
+            total = calculate_total(row["quantity"],row["price"])
+            totals.append(total)
+
+df["total"] = totals
+
+print("Sales Data:")
+for index, row in df.iterrows():
+    formatted_total = format_currency(row['total'])
+    print(f"{row['product']}: {formatted_total}")
+
+# Show grand total
+grand_total = df['total'].sum()
+formatted_grand_total = format_currency(grand_total)
+print(f"\nGrand Total: {formatted_grand_total}")
+
+
+# calculat total for each row
+
+#df["total"] = df["quantity"] * df["price"]
+#print("\nWith totals:")
+#print(df)
+
+# now make output dirctory
+
+os.makedirs("output",exist_ok=True)
+
+# now store the data into multiple files
+
+df.to_json("output/sales_analysis.json",orient='records',indent=2)
+
+df.to_csv("output/sales_analysis.csv",index=False)
+
+df.to_excel("output/sales_analysis.xlsx",index=False)
+
+print("\nFiles Saved")
+print("- output/sales_analysis.json")
+print("- output/sales_analysis.xlsx")
+print("- output/sales_analysis.csv")
+
+
+
+
+
+
+
+
+#import os
+
+#print("Current Dirctory",os.getcwd())  # current dirctory
+
+#data_path = "data/sales.csv"
+
+#if os.path.exists(data_path):
+ #           print(f"Path Finded  {data_path}")
+
+#else:
+       # print(f"Dosent Find the path {data_path}")
+        #print("Plz Ensure you are in the right Dirctory")
 
 
 # just created a script to find path of file or folder exists or not
---
+++ sale-analysis/helpers.py
@@ -0,0 +1,8 @@
+def calculate_total(quantity,price):
+            """calculate the total for single item"""
+            return quantity*price
+
+
+def format_currency(amount):
+        print(f"$ {amount:,.2f}") # 2f converts to up to 2 decimal number best for showing money
+
