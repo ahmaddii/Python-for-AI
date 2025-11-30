d1b54ac42d7d3012e5198391b7d3e86613504480
Malik Ahmad Rasheed <malikahmad6666666@gmail.com>
2025-12-01 00:30:49 +0500

    Object Oriented Programming first file creation and understanding of concepts

---
+++ oop/client.py
@@ -0,0 +1,13 @@
+class openAIClient:
+
+            def __init__(self,api_key):
+                    
+                    self.api_key = api_key
+                    self.base_url = "https://api.openai.com"
+
+            def generate(self,prompt):
+                    
+                    response = "Hello How are you"
+                    return response
+
+                    
---
+++ oop/main.py
@@ -0,0 +1,9 @@
+from client import openAIClient
+
+client = openAIClient("skashihfihihssssss")
+response = client.generate("Hello how are you")
+
+
+print(client)
+
+print(response)
